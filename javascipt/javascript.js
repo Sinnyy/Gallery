8701989510baa706aaa7857a0611edd9997cc9c4
Emmanuel <obiemmanuel97@gmail.com>
2026-01-21 08:09:11 +0100

    first push

---
+++ javascipt/javascript.js
@@ -0,0 +1,14 @@
+//console.log("Hello world")
+//alert("Hello world")
+
+// let firstName = prompt("What is your name?")
+// let age = prompt("What is your age?")
+// console.log("My name is " + firstName + ", i am " + age + " years old.")
+
+
+let amount = prompt("How much money do you have?")
+let days = prompt("Enter days")
+let total = amount * days
+console.log("You will have " + total + " after " + days + " days.")
+
+console.log("data type of total is " + typeof(total))
